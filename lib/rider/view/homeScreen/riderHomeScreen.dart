e07e84473e16a5969c9244881c87fa7eb30dfff3
thanhnt612 <nguyentruongthanh0612@gmail.com>
2024-09-01 21:29:50 +0700

    update fix:::A RenderFlex overflowed by 2.4 pixels on the bottom

--- lib/rider/view/homeScreen/riderHomeScreen.dart
+++ lib/rider/view/homeScreen/riderHomeScreen.dart
@@ -73,215 +73,226 @@ class _RiderHomeScreenState extends State<RiderHomeScreen> {
           style: AppTextStyles.heading20Bold,
         ),
       ),
-      body: ListView(
-        padding: EdgeInsets.symmetric(horizontal: 3.w, vertical: 2.h),
+      body: SingleChildScrollView(
         physics: const BouncingScrollPhysics(),
-        shrinkWrap: true,
-        children: [
-          //Where to button
-          InkWell(
-              onTap: () {},
-              child: Container(
-                padding: EdgeInsets.symmetric(horizontal: 3.w, vertical: 1.h),
-                decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(50.sp),
-                    color: greyShade3),
-                child: Row(
-                  children: [
-                    Icon(
-                      Icons.search,
-                      color: black87,
-                      size: 4.h,
-                    ),
-                    SizedBox(
-                      width: 5.w,
-                    ),
-                    Text(
-                      'Where to?',
-                      style: AppTextStyles.body14Bold,
-                    )
-                  ],
-                ),
-              )),
-          //Previous Trip Records
-          ListView.builder(
-              itemCount: 2,
-              shrinkWrap: true,
-              physics: const NeverScrollableScrollPhysics(),
-              itemBuilder: (context, index) {
-                return Container(
-                    margin: EdgeInsets.symmetric(vertical: 0.5.h),
-                    padding: EdgeInsets.symmetric(vertical: 0.5.h),
+        child: Container(
+          padding: EdgeInsets.symmetric(horizontal: 3.w, vertical: 2.h),
+          child: Column(
+            children: [
+              //Where to button
+              InkWell(
+                  onTap: () {},
+                  child: Container(
+                    padding:
+                        EdgeInsets.symmetric(horizontal: 3.w, vertical: 1.h),
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(50.sp),
+                        color: greyShade3),
                     child: Row(
                       children: [
-                        CircleAvatar(
-                          radius: 3.h,
-                          backgroundColor: greyShade3,
-                          child: Icon(
-                            Icons.location_on,
-                            color: black,
-                          ),
-                        ),
-                        SizedBox(
-                          width: 3.w,
-                        ),
-                        Expanded(
-                            child: Column(
-                          children: [
-                            Text('Main Address',
-                                style: AppTextStyles.body14Bold),
-                            Text('Main Address Description',
-                                style: AppTextStyles.small10
-                                    .copyWith(color: black38)),
-                          ],
-                        )),
-                        SizedBox(
-                          width: 3.w,
-                        ),
                         Icon(
-                          Icons.arrow_forward_ios,
-                          color: greyShade3,
-                          size: 15.sp,
+                          Icons.search,
+                          color: black87,
+                          size: 4.h,
+                        ),
+                        SizedBox(
+                          width: 5.w,
+                        ),
+                        Text(
+                          'Where to?',
+                          style: AppTextStyles.body14Bold,
                         )
                       ],
-                    ));
-              }),
+                    ),
+                  )),
+              //Previous Trip Records
+              ListView.builder(
+                  itemCount: 2,
+                  shrinkWrap: true,
+                  physics: const NeverScrollableScrollPhysics(),
+                  itemBuilder: (context, index) {
+                    return Container(
+                        margin: EdgeInsets.symmetric(vertical: 0.5.h),
+                        padding: EdgeInsets.symmetric(vertical: 0.5.h),
+                        child: Row(
+                          children: [
+                            CircleAvatar(
+                              radius: 3.h,
+                              backgroundColor: greyShade3,
+                              child: Icon(
+                                Icons.location_on,
+                                color: black,
+                              ),
+                            ),
+                            SizedBox(
+                              width: 3.w,
+                            ),
+                            Expanded(
+                                child: Column(
+                              children: [
+                                Text('Main Address',
+                                    style: AppTextStyles.body14Bold),
+                                Text('Main Address Description',
+                                    style: AppTextStyles.small10
+                                        .copyWith(color: black38)),
+                              ],
+                            )),
+                            SizedBox(
+                              width: 3.w,
+                            ),
+                            Icon(
+                              Icons.arrow_forward_ios,
+                              color: greyShade3,
+                              size: 15.sp,
+                            )
+                          ],
+                        ));
+                  }),
 
-          //Suggestion
-          SizedBox(
-            height: 4.h,
-          ),
-          Column(
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              //Suggestion
+              SizedBox(
+                height: 4.h,
+              ),
+              Column(
                 children: [
-                  Text('Suggestions', style: AppTextStyles.body14Bold),
-                  Text('See all',
-                      style: AppTextStyles.small10.copyWith(color: black38)),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text('Suggestions', style: AppTextStyles.body14Bold),
+                      Text('See all',
+                          style:
+                              AppTextStyles.small10.copyWith(color: black38)),
+                    ],
+                  ),
+                  SizedBox(
+                    height: 1.h,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: suggestions
+                        .map((e) => SizedBox(
+                              width: 20.w,
+                              height: 20.w,
+                              child: Column(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                children: [
+                                  Container(
+                                    height: 15.w,
+                                    width: 20.w,
+                                    padding: const EdgeInsets.all(2),
+                                    decoration: BoxDecoration(
+                                        borderRadius:
+                                            BorderRadius.circular(8.sp),
+                                        color: greyShadeButton),
+                                    child: Image(
+                                      image: AssetImage(e[0]),
+                                    ),
+                                  ),
+                                  Text(
+                                    e[1],
+                                    style: AppTextStyles.small10Bold
+                                        .copyWith(color: black87),
+                                  )
+                                ],
+                              ),
+                            ))
+                        .toList(),
+                  ),
                 ],
               ),
+
+              //Banner
               SizedBox(
-                height: 1.h,
+                height: 2.h,
               ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: suggestions
-                    .map((e) => SizedBox(
-                          width: 20.w,
-                          height: 20.w,
+              const Image(
+                image: AssetImage(
+                  'assets/images/promotion/promo.png',
+                ),
+              ),
+              SizedBox(
+                height: 4.h,
+              ),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'More ways to use Uber',
+                    style: AppTextStyles.body14Bold,
+                  ),
+                  SizedBox(
+                    height: 1.h,
+                  ),
+                  SizedBox(
+                    height: 20.h,
+                    child: ListView.builder(
+                      scrollDirection: Axis.horizontal,
+                      shrinkWrap: true,
+                      itemCount: moreWaysToUber.length,
+                      itemBuilder: (context, index) {
+                        return Container(
+                          margin: EdgeInsets.only(
+                              left: index == 0 ? 0 : 1.5.w,
+                              right: index == (moreWaysToUber.length - 1)
+                                  ? 0
+                                  : 1.5.w),
+                          height: 20.h,
+                          width: 65.w,
                           child: Column(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            mainAxisSize: MainAxisSize.max,
+                            crossAxisAlignment: CrossAxisAlignment.start,
                             children: [
                               Container(
-                                height: 15.w,
-                                width: 20.w,
-                                padding: const EdgeInsets.all(2),
+                                height: 15.h,
+                                width: 65.w,
                                 decoration: BoxDecoration(
-                                    borderRadius: BorderRadius.circular(8.sp),
-                                    color: greyShadeButton),
-                                child: Image(
-                                  image: AssetImage(e[0]),
-                                ),
-                              ),
-                              Text(
-                                e[1],
-                                style: AppTextStyles.small10Bold
-                                    .copyWith(color: black87),
-                              )
-                            ],
-                          ),
-                        ))
-                    .toList(),
-              ),
-            ],
-          ),
-
-          //Banner
-          SizedBox(
-            height: 2.h,
-          ),
-          const Image(
-            image: AssetImage(
-              'assets/images/promotion/promo.png',
-            ),
-          ),
-          SizedBox(
-            height: 4.h,
-          ),
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                'More ways to use Uber',
-                style: AppTextStyles.body14Bold,
-              ),
-              SizedBox(
-                height: 1.h,
-              ),
-              SizedBox(
-                height: 20.h,
-                child: ListView.builder(
-                    scrollDirection: Axis.horizontal,
-                    shrinkWrap: true,
-                    itemCount: moreWaysToUber.length,
-                    itemBuilder: (context, index) {
-                      return Container(
-                        margin: EdgeInsets.only(
-                            left: index == 0 ? 0 : 1.5.w,
-                            right: index == (moreWaysToUber.length - 1)
-                                ? 0
-                                : 1.5.w),
-                        height: 18.h,
-                        width: 65.w,
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Container(
-                              height: 15.h,
-                              width: 65.w,
-                              decoration: BoxDecoration(
-                                borderRadius: BorderRadius.circular(8.sp),
-                                image: DecorationImage(
+                                  borderRadius: BorderRadius.circular(8.sp),
+                                  image: DecorationImage(
                                     image: AssetImage(
                                       moreWaysToUber[index][0],
                                     ),
-                                    fit: BoxFit.fill),
+                                    fit: BoxFit.fill,
+                                  ),
+                                ),
                               ),
-                            ),
-                            const Spacer(),
-                            Row(
-                              children: [
-                                Text(
-                                  moreWaysToUber[index][1],
-                                  style: AppTextStyles.small12Bold,
+                              Expanded(
+                                child: Row(
+                                  children: [
+                                    Text(
+                                      moreWaysToUber[index][1],
+                                      style: AppTextStyles.small12Bold,
+                                    ),
+                                    SizedBox(
+                                      width: 3.w,
+                                    ),
+                                    Icon(
+                                      Icons.arrow_forward,
+                                      size: 2.h,
+                                      color: black87,
+                                    )
+                                  ],
                                 ),
-                                SizedBox(
-                                  width: 3.w,
-                                ),
-                                Icon(
-                                  Icons.arrow_forward,
-                                  size: 2.h,
-                                  color: black87,
-                                )
-                              ],
-                            ),
-                            SizedBox(
-                              height: 0.2.h,
-                            ),
-                            Text(
-                              moreWaysToUber[index][2],
-                              style: AppTextStyles.small10
-                                  .copyWith(color: black87),
-                            ),
-                          ],
-                        ),
-                      );
-                    }),
+                              ),
+                              SizedBox(
+                                height: 0.2.h,
+                              ),
+                              Text(
+                                moreWaysToUber[index][2],
+                                style: AppTextStyles.small10
+                                    .copyWith(color: black87),
+                              ),
+                            ],
+                          ),
+                        );
+                      },
+                    ),
+                  )
+                ],
               )
             ],
-          )
-        ],
+          ),
+        ),
       ),
     );
   }
